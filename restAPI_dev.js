7d22380dcc3d03d71984601fd740a5c4e30a649d
mattmelio <70667820+mattmelio@users.noreply.github.com>
2020-12-06 19:46:05 -0500

    Update restAPI_dev.js

--- restAPI_dev.js
+++ restAPI_dev.js
@@ -2,13 +2,14 @@
 
 var express = require("express");
 var app = express();
+var PORT = process.env.PORT||POST;
 
 var api_routes = require('./api_routes_dev.js');
 app.use('/api', api_routes);
 
 app.use('/demo',express.static('front_end'));
 
-app.listen(3000, function(){
+app.listen(PORT, function(){
 			console.log("Server is running");
 			//console.log(course)
 		   })
